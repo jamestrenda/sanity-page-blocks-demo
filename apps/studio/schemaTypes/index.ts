c57c20510c35b643d997fe58fc835a4c58022732
James Trenda <jamestrenda@gmail.com>
2025-03-17 01:48:05 -0400

    feat(studio): add studio app

---
+++ apps/studio/schemaTypes/index.ts
@@ -0,0 +1,4 @@
+import {page} from './page'
+import {tag} from './tag'
+
+export const schemaTypes = [page, tag]
---
+++ apps/studio/schemaTypes/page.ts
@@ -0,0 +1,24 @@
+import {defineField, defineType} from 'sanity'
+
+export const page = defineType({
+  name: 'page',
+  title: 'Page',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+      validation: (Rule) => Rule.required(),
+    }),
+    defineField({
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {
+        source: 'title',
+      },
+      validation: (Rule) => Rule.required(),
+    }),
+  ],
+})
