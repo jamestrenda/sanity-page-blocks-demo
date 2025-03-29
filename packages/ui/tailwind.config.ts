0cc461ca4dbb0d0e3ec7fca4a91faa5b85f49160
James Trenda <jamestrenda@gmail.com>
2025-03-29 16:26:10 -0400

    fix(ui): update content paths in tailwind config

--- packages/ui/tailwind.config.ts
+++ packages/ui/tailwind.config.ts
@@ -1,7 +1,11 @@
 import type { Config } from 'tailwindcss';
 
 const config = {
-  content: ['app/**/*.{ts,tsx}', '../../packages/ui/src/**/*.{ts,tsx}'],
+  content: [
+    'app/**/*.{ts,tsx}',
+    'components/**/*.{ts,tsx}',
+    '../../packages/ui/src/**/*.{ts,tsx}',
+  ],
 } satisfies Config;
 
 export default config;
